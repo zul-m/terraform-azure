3ff02fa133ebd6e9efd155053ffbe9368b5dfcf9
Zul.M <zul.m@icloud.com>
2022-07-01 11:29:08 +0800

    Added vnet config

--- variables.tf
+++ variables.tf
@@ -17,3 +17,8 @@ variable "resource_group_location" {
   type        = string
   description = "Choose location for your Resource Group"
 }
+
+variable "virtual_network" {
+  type        = string
+  description = "Create a name for your Virtual Network"
+}
